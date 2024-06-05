3616a5a1ed62b06d00bfd8c978b62e82d69ea5fa
Marcelo Arrarte <marcelo.arrarte.ma@gmail.com>
2024-06-05 20:51:51 -0300

    WIP

--- penca-front/src/app/app.component.ts
+++ penca-front/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NbLayoutModule, NbStepperModule } from '@nebular/theme';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NbLayoutModule, NbStepperModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
