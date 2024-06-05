1ec6e01401d963f8fd80202b76589c9a9308fb88
Marcelo Arrarte <marcelo.arrarte.ma@gmail.com>
2024-06-04 23:42:23 -0300

    Revisar DDL

--- penca-db/001_initial_design.sql
+++ penca-db/001_initial_design.sql
@@ -1,95 +1,59 @@
 CREATE TABLE equipo (
 	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(100) NOT NULL,
     pais VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE copa (
-	nombre VARCHAR(100) PRIMARY KEY,
-    fecha_inicio DATETIME NOT NULL,
-    fecha_fin DATETIME NOT NULL,
-    pais_anfitrion VARCHAR(100) NOT NULL,
-    id_campeon INT UNSIGNED,
-    id_subcampeon INT UNSIGNED,
-    CONSTRAINT fk_copa_campeon FOREIGN KEY (id_campeon) REFERENCES equipo (id),
-    CONSTRAINT fk_copa_subcampeon FOREIGN KEY (id_subcampeon) REFERENCES equipo (id)
-);
-
 CREATE TABLE fase (
-	nombre VARCHAR(50),
-    nombre_copa VARCHAR(100),
-    CONSTRAINT fk_fase_copa FOREIGN KEY (nombre_copa) REFERENCES copa (nombre),
-    CONSTRAINT pk_fase PRIMARY KEY (nombre, nombre_copa)
-);
-
-CREATE TABLE penca (
-	nombre VARCHAR(50) PRIMARY KEY,
-    fecha_inicio_inscripcion DATETIME NOT NULL,
-    fecha_fin_inscripcion DATETIME NOT NULL,
-    nombre_copa VARCHAR(100) NOT NULL,
-    CONSTRAINT fk_penca_copa FOREIGN KEY (nombre_copa) REFERENCES copa (nombre)
-);
-
-CREATE TABLE alumno (
-	documento VARCHAR(15) PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE
+	nombre VARCHAR(50) PRIMARY KEY
 );
 
 CREATE TABLE carrera (
 	nombre VARCHAR(100) PRIMARY KEY
 );
 
+CREATE TABLE usuario (
+	documento VARCHAR(15) PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE `admin` (
+	documento_usuario VARCHAR(15) PRIMARY KEY,
+    CONSTRAINT fk_admin_usuario FOREIGN KEY (documento_usuario) REFERENCES usuario (documento)
+);
+
+CREATE TABLE alumno (
+	documento_usuario VARCHAR(15) PRIMARY KEY,
+    id_campeon INT UNSIGNED NOT NULL,
+    id_subcampeon INT UNSIGNED NOT NULL,
+    CONSTRAINT fk_alumno_usuario FOREIGN KEY (documento_usuario) REFERENCES usuario (documento),
+    CONSTRAINT fk_alumno_campeon FOREIGN KEY (id_campeon) REFERENCES equipo (id),
+    CONSTRAINT fk_alumno_subcampeon FOREIGN KEY (id_subcampeon) REFERENCES equipo (id)
+);
+
 CREATE TABLE cursa (
 	documento_alumno VARCHAR(15) NOT NULL,
     nombre_carrera VARCHAR(100) NOT NULL,
     CONSTRAINT pk_cursa PRIMARY KEY (documento_alumno, nombre_carrera),
-    CONSTRAINT fk_cursa_alumno FOREIGN KEY (documento_alumno) REFERENCES alumno (documento),
+    CONSTRAINT fk_cursa_alumno FOREIGN KEY (documento_alumno) REFERENCES alumno (documento_usuario),
     CONSTRAINT fk_cursa_carrera FOREIGN KEY (nombre_carrera) REFERENCES carrera (nombre)
-);
-
-CREATE TABLE usuario (
-	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    documento_alumno VARCHAR(15) NOT NULL,
-    nombre_penca VARCHAR(50) NOT NULL,
-    contraseña VARCHAR(200) NOT NULL,
-    CONSTRAINT unq_alumno_penca UNIQUE (documento_alumno, nombre_penca),
-    CONSTRAINT fk_usuario_alumno FOREIGN KEY (documento_alumno) REFERENCES alumno (documento),
-    CONSTRAINT fk_usuario_penca FOREIGN KEY (nombre_penca) REFERENCES penca (nombre)
-);
-
-CREATE TABLE `admin` (
-	id_usuario INT UNSIGNED PRIMARY KEY,
-    CONSTRAINT fk_admin_usuario FOREIGN KEY (id_usuario) REFERENCES usuario (id)
-);
-
-CREATE TABLE participante (
-	id_usuario INT UNSIGNED PRIMARY KEY,
-    id_campeon INT UNSIGNED NOT NULL,
-    id_subcampeon INT UNSIGNED NOT NULL,
-    CONSTRAINT fk_participante_usuario FOREIGN KEY (id_usuario) REFERENCES usuario (id),
-    CONSTRAINT fk_participante_campeon FOREIGN KEY (id_campeon) REFERENCES equipo (id),
-    CONSTRAINT fk_participante_subcampeon FOREIGN KEY (id_subcampeon) REFERENCES equipo (id)
 );
 
 CREATE TABLE partido (
 	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     fecha_hora DATETIME NOT NULL,
-    finalizado TINYINT(1) NOT NULL,
-    numero_en_torneo INT UNSIGNED,
-    nombre_copa VARCHAR(100) NOT NULL,
+    numero_en_torneo INT UNSIGNED UNIQUE,
     nombre_fase VARCHAR(50) NOT NULL,
-    CONSTRAINT unq_numero_torneo_copa UNIQUE (numero_en_torneo, nombre_copa),
-    CONSTRAINT fk_partido_fase FOREIGN KEY (nombre_copa, nombre_fase) REFERENCES fase (nombre, nombre_copa)
+    CONSTRAINT fk_partido_fase FOREIGN KEY (nombre_fase) REFERENCES fase (nombre)
 );
 
 CREATE TABLE notifica (
 	id_partido INT UNSIGNED,
-    id_usuario INT UNSIGNED,
+    documento_alumno VARCHAR(15),
     fecha_envio DATETIME,
     mensaje TEXT NOT NULL,
-    CONSTRAINT pk_notifica PRIMARY KEY (id_partido, id_usuario),
-    CONSTRAINT fk_notifica_participante FOREIGN KEY (id_usuario) REFERENCES participante (id_usuario),
+    CONSTRAINT pk_notifica PRIMARY KEY (id_partido, documento_alumno),
+    CONSTRAINT fk_notifica_alumno FOREIGN KEY (documento_alumno) REFERENCES alumno (documento_usuario),
     CONSTRAINT fk_notifica_partido FOREIGN KEY (id_partido) REFERENCES partido (id)
 );
 
@@ -105,9 +69,9 @@ CREATE TABLE juega (
 CREATE TABLE predice (
 	id_equipo INT UNSIGNED,
     id_partido INT UNSIGNED,
-    id_participante INT UNSIGNED,
+    documento_alumno VARCHAR(15),
     goles INT UNSIGNED,
-    CONSTRAINT pk_predice PRIMARY KEY (id_equipo, id_partido, id_participante),
+    CONSTRAINT pk_predice PRIMARY KEY (id_equipo, id_partido, documento_alumno),
     CONSTRAINT fk_predice_juega FOREIGN KEY (id_equipo, id_partido) REFERENCES juega (id_equipo, id_partido),
-    CONSTRAINT fk_predice_participante FOREIGN KEY (id_participante) REFERENCES participante (id_usuario)
-)
+    CONSTRAINT fk_predice_alumno FOREIGN KEY (documento_alumno) REFERENCES alumno (documento_usuario)
+);
