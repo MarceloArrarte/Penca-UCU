40ea0552349dad7bbc33f48e52f6f7650fdf1ba4
FrancoDest <franco.destefano@correo.ucu.edu.uy>
2024-06-10 19:20:41 -0300

    Añado estructura login y sign-up
    [run ci]

--- penca-front/src/app/app.module.ts
+++ penca-front/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule, NbSidebarModule, NbCardModule, NbInputModule, NbButtonModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
