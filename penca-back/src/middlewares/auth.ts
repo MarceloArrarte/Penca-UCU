cb619e068b97bdc4de62918fa557ec083d1e3aea
Joelit0 <joelalagarcia@gmail.com>
2024-06-19 21:43:16 -0300

    Add auth middleware and some error handling

---
+++ penca-back/src/middlewares/auth.ts
@@ -0,0 +1,43 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+const secretKey = process.env.JWT_SECRET || 'your-secret-key';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: DecodedToken;
+    }
+  }
+}
+
+interface DecodedToken {
+  document: number;
+  email: string;
+  role: String;
+}
+
+export const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1]
+
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: 'Token is not valid or has expired' });
+    }
+
+    req.user = decoded as DecodedToken;
+
+    next();
+  });
+};
+
+export const authorizeAdmin = (req: Request, res: Response, next: NextFunction) => {
+  if (req.user && req.user.role == 'admin') {
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+};
--- penca-back/src/models/predictionModel.ts
+++ penca-back/src/models/predictionModel.ts
@@ -14,9 +14,30 @@ interface MatchPrediction {
 
 const predictTeamsGoalsForMatch = (userDocument: number, matchId: number, predictions: Prediction[]): Promise<MatchPrediction> => {
   return new Promise((resolve, reject) => {
-
     db.beginTransaction(err => {
       if (err) { return reject(err); }
+
+      db.query(`SELECT fecha_hora FROM partido where id = ?`, [matchId], (err, results) => {
+        if (err) { return db.rollback(() => { reject(err); }); }
+
+        const match = results as RowDataPacket[];
+
+        if (!match[0]) {
+          db.rollback(() => {
+            reject('Match does not exists')
+          });
+        }
+
+        const matchDate = new Date(match[0].fecha_hora);
+        const currentDateUTC = new Date();
+        const currentDate = new Date(currentDateUTC.getTime() - (3 * 60 * 60 * 1000));
+
+        if (matchDate <= currentDate) {
+          return db.rollback(() => {
+            reject('Cannot predict a match that has already started');
+          });
+        }
+      });
 
       const matchPrediction : MatchPrediction = {
         userDocument: userDocument,
@@ -26,6 +47,18 @@ const predictTeamsGoalsForMatch = (userDocument: number, matchId: number, predic
 
       const predictionPromises = predictions.map(prediction => {
         return new Promise<void>((resolve, reject) => {
+          db.query('SELECT * FROM equipo where id = ?', [prediction.teamId], (err, results) => {
+            if (err) { return db.rollback(() => { reject(err); }); }
+
+            const team = results as RowDataPacket[];
+
+            if (!team[0]) {
+              db.rollback(() => {
+                reject(`Team(${prediction.teamId}) does not exists`)
+              });
+            }
+          });
+
           db.query(`
             INSERT INTO predice (id_equipo, id_partido, documento_alumno, goles)
             VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE goles = VALUES(goles)
