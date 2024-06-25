663235cdf9d725bfcc78fd12549a60979e85d4f9
Joelit0 <joelalagarcia@gmail.com>
2024-06-25 00:29:02 -0300

    Check champion and runner up prediction when calculate users scores

--- penca-back/src/models/userModel.ts
+++ penca-back/src/models/userModel.ts
@@ -25,32 +25,31 @@ interface UserScore {
 }
 
 const getAllUserScores = (): Promise<UserScore[]> => {
-  return new Promise((resolve, reject) => { 
-    db.query('SELECT documento, nombre FROM alumno a INNER JOIN usuario u WHERE u.documento = a.documento_usuario;', (err, results) => {
+  return new Promise((resolve, reject) => {
+    db.query('SELECT documento, nombre, documento_usuario, id_campeon, id_subcampeon FROM alumno a INNER JOIN usuario u WHERE u.documento = a.documento_usuario;', (err, results) => {
       if (err) { return reject(err); }
 
       const users = results as RowDataPacket[];
-      
-      console.log(users);
 
-      const usersRanking: UserScore[] = users.map(row => {  
-        return {
-          userDocument: row.documento,
-          name: row.nombre,
-          score: 0
-        };
-      });
-    
-      const userScoresPromises = usersRanking.map(userScore => {
+      const usersScores: UserScore[] = users.map(row => ({
+        userDocument: row.documento,
+        name: row.nombre,
+        score: 0,
+      }));
+
+      const userScoresPromises = users.map(user => {
         return new Promise<void>((resolve, reject) => {
-          db.query(`SELECT p.id AS id_partido, e.id AS id_equipo, pr.goles AS prediccion_goles, j.goles
+          const userScore = usersScores.find(userScore => userScore.userDocument === user.documento_usuario);
+
+          db.query(
+            `SELECT p.id AS id_partido, e.id AS id_equipo, pr.goles AS prediccion_goles, j.goles
             FROM partido p
-            INNER JOIN juega j on j.  id_partido = p.id
-            INNER JOIN equipo e on e.id = j.id_equipo
-            LEFT JOIN  predice pr ON pr.id_equipo = j.id_equipo AND pr.id_partido = j.id_partido
+            INNER JOIN juega j ON j.id_partido = p.id
+            INNER JOIN equipo e ON e.id = j.id_equipo
+            LEFT JOIN predice pr ON pr.id_equipo = j.id_equipo AND pr.id_partido = j.id_partido
             WHERE j.goles IS NOT NULL AND pr.goles IS NOT NULL AND pr.documento_alumno = ?
-            ORDER BY p.id;`, [userScore.userDocument],(err, results) => {
-              if (err) { return reject(err); }
+            ORDER BY p.id;`, [user.documento_usuario], (err, results) => {
+              if (err) { return reject(err);}
 
               const rows = results as RowDataPacket[];
 
@@ -61,56 +60,89 @@ const getAllUserScores = (): Promise<UserScore[]> => {
                   matchId: row.id_partido,
                   teamId: row.id_equipo,
                   goalsPrediction: row.prediccion_goles,
-                  goals: row.goles
+                  goals: row.goles,
                 };
-    
+
                 if (!predictionsMap[prediction.matchId]) {
                   predictionsMap[prediction.matchId] = [];
                 }
-    
+
                 predictionsMap[prediction.matchId].push(prediction);
               });
 
               for (const matchId in predictionsMap) {
                 const matchPredictions = predictionsMap[matchId];
-    
-                if (matchPredictions.length === 2) {
+
+                if (matchPredictions.length === 2 && userScore) {
                   const team1 = matchPredictions[0];
                   const team2 = matchPredictions[1];
-    
+
                   if (team1 && team2) {
                     const actualResult = team1.goals - team2.goals;
                     const predictedResult = team1.goalsPrediction - team2.goalsPrediction;
 
-                    if (team1.goals == team1.goalsPrediction && team2.goals == team2.goalsPrediction) {
+                    if (team1.goals === team1.goalsPrediction && team2.goals === team2.goalsPrediction) {
                       userScore.score += 4;
                     } else if (
-                      (actualResult > 0 && predictedResult > 0) || 
-                      (actualResult < 0 && predictedResult < 0) || 
-                      (actualResult === 0 && predictedResult === 0) ) {
-                        userScore.score += 2;
+                      (actualResult > 0 && predictedResult > 0) ||
+                      (actualResult < 0 && predictedResult < 0) ||
+                      (actualResult === 0 && predictedResult === 0)
+                    ) {
+                      userScore.score += 2;
                     }
                   }
                 }
               }
 
-              resolve()
-          });
+              db.query(`SELECT j.id_equipo, j.goles FROM partido p INNER JOIN juega j ON j.id_partido = p.id 
+                WHERE p.nombre_fase = "Final" AND j.goles IS NOT NULL;`, (err, results) => {
+                  if (err) { return reject(err); }
+
+                  const finalMatchResults = results as RowDataPacket[];
+
+                  if (finalMatchResults.length !== 2) { return resolve(); }
+
+                  let championId: number;
+                  let runnerUpId: number;
+
+                  if (finalMatchResults[0].goles > finalMatchResults[1].goles) {
+                    championId = finalMatchResults[0].id_equipo;
+                    runnerUpId = finalMatchResults[1].id_equipo;
+                  } else if (finalMatchResults[0].goles < finalMatchResults[1].goles) {
+                    championId = finalMatchResults[1].id_equipo;
+                    runnerUpId = finalMatchResults[0].id_equipo;
+                  } else {
+                    championId = 0;
+                    runnerUpId = 0;
+                  }
+
+                  if (championId === user.id_campeon && userScore) {
+                    userScore.score += 10;
+                  }
+
+                  if (runnerUpId === user.id_subcampeon && userScore) {
+                    userScore.score += 5;
+                  }
+
+                  resolve();
+                }
+              );
+            }
+          );
         });
       });
 
-      Promise.all(userScoresPromises)
-      .then(() => {
-        usersRanking.sort((a, b) => b.score - a.score);
+      Promise.all(userScoresPromises).then(() => {
+        usersScores.sort((a, b) => b.score - a.score);
 
-        resolve(usersRanking);
+        resolve(usersScores);
       })
       .catch(err => {
         reject('Error');
       });
     });
   });
-};
+}
 
 const getUserByEmail = (email: string): Promise<User> => {
   return new Promise((resolve, reject) => {
